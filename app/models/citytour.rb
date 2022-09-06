ff9f70674e4fd4ed3decd7e54a850a9d126c05a4
oliver stoislow <oliver.stoislow@gmx.de>
2022-09-06 16:46:31 +0200

    Seeds work now and also acts_as_taggable_on thanks to George

--- app/models/citytour.rb
+++ app/models/citytour.rb
@@ -1,2 +1,3 @@
 class Citytour < ApplicationRecord
+  belongs_to :guide, foreign_key: 'user_id', class_name: 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_135015) do
-#ActiveRecord::Schema[7.0].define(version: 2022_09_06_133523) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_143017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_135015) do
     t.decimal "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sights_on_user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
@@ -113,5 +112,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_135015) do
   add_foreign_key "citytour_sights", "citytours"
   add_foreign_key "citytour_sights", "sights"
   add_foreign_key "citytours", "users"
+  add_foreign_key "sights", "users"
   add_foreign_key "taggings", "tags"
 end
