b793801993297b24a51b521c9de007d7a5c5bc1e
Sydel Chayat <mayoindonela@yahoo.com>
2022-09-06 16:31:05 +0300

    almost done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.citytour = Citytour.find(params[:city_tour_id])
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
 end
