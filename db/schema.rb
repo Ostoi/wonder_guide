f638a64ece72166db7639275ac498e121864290a
Sydel Chayat <mayoindonela@yahoo.com>
2022-09-07 16:52:52 +0300

    homepage not yet done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_135015) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_143017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,10 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_135015) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.bigint "guide_id", null: false
     t.index ["guide_id"], name: "index_citytours_on_guide_id"
-    t.index ["user_id"], name: "index_citytours_on_user_id"
   end
 
   create_table "sights", force: :cascade do |t|
