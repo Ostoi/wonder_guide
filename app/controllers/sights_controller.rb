b61af86a261b865d407db5efa4e6ce0818741ad6
oliver stoislow <oliver.stoislow@gmx.de>
2022-09-07 15:47:17 +0200

    We now have a map that works.

--- app/controllers/sights_controller.rb
+++ app/controllers/sights_controller.rb
@@ -1,7 +1,25 @@
 class SightsController < ApplicationController
 
   def index
-    @sights = sight.all
+    @sights = Sight.all
+
+    if params[:longitude] && params[:latitude]
+      @sights = Sight.near([params[:latitude], params[:longitude]], 50)
+    end
+
+    if params[:address]
+      @sights = Sight.near(params[:address], 50)
+    end
+
+    # The `geocoded` scope filters only sights with coordinates
+    @markers = @sights.geocoded.map do |sight|
+      {
+        lat: sight.latitude,
+        lng: sight.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {sight: sight}),
+        image_url: "https://images.unsplash.com/photo-1644815410301-71ab7334670e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8RnpvM3p1T0hONnd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+      }
+    end
   end
 
   def new
