09c14de2e3f08976ed16ee78dc7d4d03d3c9a28b
oliver stoislow <oliver.stoislow@gmx.de>
2022-09-09 12:21:46 +0200

    Show page works again after commenting out new lines of code and after putting old lines of code in

--- app/controllers/sights_controller.rb
+++ app/controllers/sights_controller.rb
@@ -1,12 +1,8 @@
 class SightsController < ApplicationController
-  before_action :set_sight, only: [:show, :edit, :update, :destroy]
-
-  def set_sight
-    @sight = controller_path.classify.constantize.find(params[:id])
-   end
 
   def index
     @sights = Sight.all
+    @a_sight = Sight.last
 
 
     if params[:longitude] && params[:latitude]
@@ -54,13 +50,8 @@ class SightsController < ApplicationController
   end
 
   def destroy
-    Sight.destroy(params[:id])
-
-    # def destroy
-    #   @sight = Sight.find(params[:id])
-    #   @sight = Sight.destroy
-
-    redirect_to :controller => 'sights', :action => 'index'
+    @sight.destroy(params[:id])
+    redirect_to :controller => 'sight', :action => 'index'
   end
 
   private
