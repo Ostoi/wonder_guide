449e15e6f3018239ceda54f5564b9516639769c2
oliver stoislow <oliver.stoislow@gmx.de>
2022-09-09 11:50:52 +0200

    Push my working progress as we said in the morning meeting

--- app/models/citytour.rb
+++ app/models/citytour.rb
@@ -1,4 +1,5 @@
 class Citytour < ApplicationRecord
   belongs_to :guide, foreign_key: 'guide_id', class_name: 'User'
   has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :booking
 end
