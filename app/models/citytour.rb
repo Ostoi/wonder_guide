9871b6826a4577337132957854b2e56c860b9154
Melchior Brincken <christoph.brincken@gmx.at>
2022-09-12 14:20:51 +0200

    user_guide

--- app/models/citytour.rb
+++ app/models/citytour.rb
@@ -1,4 +1,5 @@
 class Citytour < ApplicationRecord
+  has_one_attached :photo
   belongs_to :guide, foreign_key: 'guide_id', class_name: 'User'
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
