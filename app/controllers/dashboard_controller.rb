481b95fe0d7a8b0cff201cf857c3a41f013e94ee
Sydel Chayat <mayoindonela@yahoo.com>
2022-09-12 13:15:10 +0300

    updated

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,11 @@
 class DashboardController < ApplicationController
   def dashboard
+    #FOR GUIDE
     @bookings = current_user.bookings
     @citytours = current_user.citytours
     @guide_bookings = Booking.where(citytour: @citytours)
-    # @customer_bookings = current_user.bookings
-    # @messages = ?
+    # FOR TRAVELLER
+    @pending_bookings = current_user.bookings.where(status: "pending")
+    @upcoming_citytours = Citytour.where(bookings: current_user.bookings.where(status: "accepted"))
   end
 end
